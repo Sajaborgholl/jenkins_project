98b4e959e06f46533f8dc271ee443966c0726474
sajaborgholl <saja.borgholl@hotmail.com>
2024-11-20 20:49:24 +0200

    message

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
-import unittest
 from app import greet
+import unittest
+import sys
+import os
+
+# Dynamically add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 
 
 class TestApp(unittest.TestCase):
